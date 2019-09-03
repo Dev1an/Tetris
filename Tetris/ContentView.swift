cef3c4d31c4aec50b4d5c4b043c83ea1d525d9e0
Damiaan Dufaux <damiaan@dpro.be>
2019-09-03 18:08:55 +0200

    Initial Commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by Damiaan on 03/09/2019.
+//  Copyright © 2019 dPro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
